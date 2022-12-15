a336186d1172f2b3abe52c5d37b19f8414da7f99
Saurabh Rana <106344770+saurabhrana7699@users.noreply.github.com>
2022-12-15 22:41:44 +0530

    finally hogya
    checking on

---
+++ ubiquitous-pancake-7880/CustomerLog/src/main/java/com/masai/exception/CustomerEception.java
@@ -0,0 +1,11 @@
+package com.masai.exception;
+
+public class CustomerEception  extends Exception{
+	public CustomerEception() {
+		
+	}
+
+public CustomerEception(String msg) {
+		super(msg);
+	}
+}
---
+++ ubiquitous-pancake-7880/src/main/java/com/masai/Unit6Eval4springBootQue11Application.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit6Eval4springBootQue11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit6Eval4springBootQue11Application.class, args);
+	}
+
+}
